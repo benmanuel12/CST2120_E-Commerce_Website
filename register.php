c585286ab2e96a90861950cea0421c5ef1a49a18
Benjamin Manuel <contact.ben.manuel@gmail.com>
2020-01-18 15:52:09 +0000

    Initial Commit

---
+++ register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body id="register">
+    <?php //include 'header.php';?>
+    <div id="mainbody">
+        <div id="mainpanel">
+            <div id="title">Register</div>
+            <form>
+            First Name:
+            <input id="firstname" type="text" name="firstname"><br> <br> Last Name:
+            <input id="lastname" type="text" name="lastname"><br> <br> Email: 
+            <input id="email" type="email" name="email"><br> <br> Password: 
+            <input id="password" type="password" name="password"><br> <br> Address:
+            <input id="address" type="text" name="address"><br> <br> Telephone: 
+            <input id="number" type="tel" name="number"><br>
+            <button type="button" onclick="register()">Register</button>
+        </form>
+</div>
+        
+    </div>
+    <?php //include 'footer.php';?>
+    <script src="scripts/register.js"></script>
+</body>
+
+</html
---
+++ signin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body id="signin">
+    <?php //include 'header.php';?>
+    <div id="mainbody">
+        <div id="leftpanel">
+            <div id="registerlinkbutton"><a href='register.php'>Register ></a></div>
+        </div>
+        <div id="rightpanel">
+            <div id="title">Sign In</div>
+            <form>
+                Email: <br>
+                <input id="email" type="email" name="email"><br> <br> Password: <br>
+                <input id="password" type="password" name="password"><br>
+                <button type="button">Sign In</button>
+            </form>
+        </div>
+    </div>
+    <?php //include 'footer.php';?>
+    <script src="scripts/signin.js"></script>
+</body>
+
+</html>
