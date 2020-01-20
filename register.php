120e7d69e660734ff3d9642fba024204bc980252
Benjamin Manuel <contact.ben.manuel@gmail.com>
2020-01-20 16:46:44 +0000

    added bootstrap and started to reformat to fit group plan

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -6,27 +6,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 </head>
 
 <body id="register">
-    <?php //include 'header.php';?>
+    <?php //include 'header.php';
+    ?>
     <div id="mainbody">
         <div id="mainpanel">
             <div id="title">Register</div>
-            <form>
-            First Name:
-            <input id="firstname" type="text" name="firstname"><br> <br> Last Name:
-            <input id="lastname" type="text" name="lastname"><br> <br> Email: 
-            <input id="email" type="email" name="email"><br> <br> Password: 
-            <input id="password" type="password" name="password"><br> <br> Address:
-            <input id="address" type="text" name="address"><br> <br> Telephone: 
-            <input id="number" type="tel" name="number"><br>
-            <button type="button" onclick="register()">Register</button>
-        </form>
-</div>
-        
+            <form class="form-horizontal" action="">
+                <div class="form-group form-inline">
+                    <label class="control-label col-sm-2" for="firstname">First Name:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="firstname" placeholder="First name" name="firstname">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="lastname">Last Name:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="lastname" placeholder="Last name" name="lastname">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Email:</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="password">Password:</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="address">Address:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="address" placeholder="Address" name="address">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="telephone">Telephone Number:</label>
+                    <div class="col-sm-10">
+                        <input type="tel" class="form-control" id="telephone" placeholder="Telephone Number">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-default" onclick="register()">Register</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-    <?php //include 'footer.php';?>
+    <?php //include 'footer.php';
+    ?>
     <script src="scripts/register.js"></script>
 </body>
 
--- signin.php
+++ signin.php
@@ -6,25 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 </head>
 
 <body id="signin">
-    <?php //include 'header.php';?>
+    <?php //include 'header.php';
+    ?>
     <div id="mainbody">
         <div id="leftpanel">
             <div id="registerlinkbutton"><a href='register.php'>Register ></a></div>
         </div>
         <div id="rightpanel">
             <div id="title">Sign In</div>
-            <form>
-                Email: <br>
-                <input id="email" type="email" name="email"><br> <br> Password: <br>
-                <input id="password" type="password" name="password"><br>
-                <button type="button" onclick="signin()">Sign In</button>
+            <form class="form-horizontal" action="">
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Email:</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="email" placeholder="Enter email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="pwd">Password:</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
-    <?php //include 'footer.php';?>
+    <?php //include 'footer.php';
+    ?>
     <script src="scripts/signin.js"></script>
 </body>
 
